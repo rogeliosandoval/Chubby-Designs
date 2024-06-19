6e937fc4a28aa54bd01bb9065d9a150dea76f143
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-19 15:22:17 -0500

    import angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,33 @@ import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 //Pages
 import { AppComponent } from './app.component'
 import { Home } from './pages/home/home.component'
-import { Contact } from './pages/contact/contact.component';
+import { Contact } from './pages/contact/contact.component'
+
+//Angular Material
+import { MatDividerModule } from '@angular/material/divider'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatButtonModule } from '@angular/material/button'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
