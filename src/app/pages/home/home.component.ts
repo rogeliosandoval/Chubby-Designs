12a170da6415534ab6ecb5254cab16a13498aaea
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-20 15:25:59 -0500

    update the home page with content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core'
 })
 
 export class Home {
+  public preview: number = 0
 
+  public incrementPreview(): void {
+    this.preview = (this.preview + 1) % 2;
+  }
+
+  public decrementPreview(): void {
+    if (this.preview === 0) {
+      this.preview = 1
+    } else {
+      this.preview--
+    }
+  }
 }
