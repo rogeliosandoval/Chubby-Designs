d59783f407f9da30f7651848dee0a75f5418f085
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-22 14:55:54 -0500

    update the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'home',
@@ -6,11 +6,21 @@ import { Component } from '@angular/core'
   styleUrls: ['home.component.scss']
 })
 
-export class Home {
+export class Home implements OnInit {
+  @ViewChild('mission') mission!: ElementRef;
+  @ViewChild('facts') facts!: ElementRef;
   public preview: number = 0
+  public showMission: boolean = false
+  public showFacts: boolean = false
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.onViewportScroll()
+    }, 100)
+  }
 
   public incrementPreview(): void {
-    this.preview = (this.preview + 1) % 2;
+    this.preview = (this.preview + 1) % 2
   }
 
   public decrementPreview(): void {
@@ -18,6 +28,24 @@ export class Home {
       this.preview = 1
     } else {
       this.preview--
+    }
+  }
+
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+    const windowHeight = window.innerHeight;
+    const boundingRectMission = this.mission.nativeElement.getBoundingClientRect();
+    const boundingRectFacts = this.facts.nativeElement.getBoundingClientRect();
+
+    console.log('div', boundingRectFacts.top)
+    console.log('div', boundingRectFacts.bottom)
+
+    if (boundingRectMission.top >= 0 && boundingRectMission.bottom <= windowHeight + 100) {
+      setTimeout(() => { this.showMission = true; }, 500);
+    } 
+
+    if (boundingRectFacts.top >= 0 && boundingRectFacts.bottom <= windowHeight + 200) {
+      setTimeout(() => { this.showFacts = true; }, 500);
     }
   }
 }
