41d81004afdded63e55539eab84fd61f68f68b1a
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-23 02:33:20 -0500

    update home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,16 +7,23 @@ import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular
 })
 
 export class Home implements OnInit {
-  @ViewChild('mission') mission!: ElementRef;
-  @ViewChild('facts') facts!: ElementRef;
+  @ViewChild('mission') mission!: ElementRef
+  @ViewChild('facts') facts!: ElementRef
+  @ViewChild('contact') contact!: ElementRef
   public preview: number = 0
   public showMission: boolean = false
   public showFacts: boolean = false
+  public showContact: boolean = false
+  public mobile: boolean = false
 
   ngOnInit(): void {
     setTimeout(() => {
       this.onViewportScroll()
     }, 100)
+
+    if (window.innerWidth <= 768) {
+      this.mobile = true
+    }
   }
 
   public incrementPreview(): void {
@@ -34,11 +41,9 @@ export class Home implements OnInit {
   @HostListener('document:scroll', ['$event'])
   public onViewportScroll() {
     const windowHeight = window.innerHeight;
-    const boundingRectMission = this.mission.nativeElement.getBoundingClientRect();
-    const boundingRectFacts = this.facts.nativeElement.getBoundingClientRect();
-
-    console.log('div', boundingRectFacts.top)
-    console.log('div', boundingRectFacts.bottom)
+    const boundingRectMission = this.mission.nativeElement.getBoundingClientRect()
+    const boundingRectFacts = this.facts.nativeElement.getBoundingClientRect()
+    const boundingRectContact= this.contact.nativeElement.getBoundingClientRect()
 
     if (boundingRectMission.top >= 0 && boundingRectMission.bottom <= windowHeight + 100) {
       setTimeout(() => { this.showMission = true; }, 500);
@@ -46,6 +51,10 @@ export class Home implements OnInit {
 
     if (boundingRectFacts.top >= 0 && boundingRectFacts.bottom <= windowHeight + 200) {
       setTimeout(() => { this.showFacts = true; }, 500);
+    }
+
+    if (boundingRectContact.top >= 0 && boundingRectContact.bottom <= windowHeight + 300) {
+      setTimeout(() => { this.showContact = true; }, 500);
     }
   }
 }
