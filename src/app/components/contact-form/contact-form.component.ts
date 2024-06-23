4e4386f5cc4644fb6b4e6e4301b9f57cf8f34349
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-06-22 19:53:09 -0500

    add contact page

---
+++ src/app/components/contact-form/contact-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, ViewChild, inject } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: 'contact-form.component.html',
+  styleUrls: ['contact-form.component.scss']
+})
+
+export class ContactForm {
+  @ViewChild('formRef') formRef: any
+  private matSnack = inject(MatSnackBar)
+  private http = inject(HttpClient)
+  private url = 'https://api.web3forms.com/submit'
+  public sending: boolean = false
+  public contactForm = new FormGroup({
+    access_key: new FormControl('12ee3a8d-fb2b-466e-a975-4f6b29a0887d'),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subject: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+  })
+
+  public sendMessage(): void {
+    this.sending = true
+    setTimeout(() => {
+      this.http.post(this.url, this.contactForm.value).subscribe({
+        next: response => {
+          this.contactForm.reset()
+          this.formRef.resetForm()
+          this.sending = false
+          this.matSnack.open('Message sent. Thank you!', '👍', {
+            duration: 10000,
+            verticalPosition: 'bottom',
+            horizontalPosition: 'center'
+          })
+        },
+        error: err => {
+          this.sending = false
+          this.matSnack.open('There was an error. Please try again.', '👍', {
+            duration: 10000,
+            verticalPosition: 'bottom',
+            horizontalPosition: 'center'
+          })
+          console.log(err)
+          throw err
+        }
+      })
+    }, 2000)
+  }
+}
